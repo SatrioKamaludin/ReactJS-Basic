ede7e66165a8ecafa0d91a3ebc5c3ff96efbc0a5
Satrio Kamaludin <satriokamaludin@gmail.com>
2023-06-16 04:44:16 +0700

    Event

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import { Component } from "react";
-import Button from "./Button";
 
 class App extends Component {
+    change = () => {
+        console.log("ok")
+    }
     render(){
         return(
             <div>
                 <h1>Hello World</h1>
-                <Button color="biru"/>
+                <button onClick={this.change}>Click this and see console log</button>
             </div>
             //<div></div> //Parsing error: Adjacent JSX elements must be wrapped in an enclosing tag.
         )
