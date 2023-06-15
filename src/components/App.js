47d31aac97c9e68c11a581cd397bf3a706f7a440
Satrio Kamaludin <satriokamaludin@gmail.com>
2023-06-15 20:12:28 +0700

    function component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,29 @@
 import { Component } from "react";
+import React from 'react'
 
-class App extends Component {
-    render(){
-        return(
-            <div>
-                <h1>Hello World</h1>
-                <div>div 2</div>
-                <p>test</p>
-                <div>div 3</div>
-            </div>
-            //<div></div> //Parsing error: Adjacent JSX elements must be wrapped in an enclosing tag.
-        )
-    }
+// class App extends Component {
+//     render(){
+//         return(
+//             <div>
+//                 <h1>Hello World</h1>
+//                 <div>div 2</div>
+//                 <p>test</p>
+//                 <div>div 3</div>
+//             </div>
+//             //<div></div> //Parsing error: Adjacent JSX elements must be wrapped in an enclosing tag.
+//         )
+//     }
+// }
+
+const App = () => {
+    return(
+        <div>
+            <h1>Hello World</h1>
+            <div>div 2</div>
+            <p>test</p>
+            <div>div 3</div>
+        </div>
+    )
 }
 
 export default App;
